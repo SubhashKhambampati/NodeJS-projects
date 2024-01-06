8378d1bd2a5e5d6321cab7af8b23af7d6fafd56c
SubhashKhambampati <subhashkhambampati74@gmail.com>
2024-01-06 16:27:34 +0530

    completed register

--- Project1/mycontacts-backend/controllers/userController.js
+++ Project1/mycontacts-backend/controllers/userController.js
@@ -2,7 +2,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require("../models/userModel");
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 const token = require('jsonwebtoken');
 
 
@@ -45,6 +45,30 @@ const registerUser = asyncHandler(async (req,res)=>{
 });
 
 const loginUser = asyncHandler(async (req,res)=>{
+    const { email , password} = req.body;
+    if (!password || !email) {
+        res.status(400);
+        throw new Error("All fields are mandatory.");
+
+
+    }
+    const user = await User.findOne({email});
+    if (user && (await bcrypt.compare(password , user.password))){
+
+        const accessToken = jwt.sign({
+            user:{
+                username:user.username,
+                email:user.email,
+                id:user.id
+            },
+        },process.env.ACCESS_TOKEN_SECRET ,{expiresIn:'1d'});
+
+    }
+    else{
+        res.status(401);
+        throw new Error("Password and email are not valid");
+
+    }
     res.json({message : "login user"});
 
 });
